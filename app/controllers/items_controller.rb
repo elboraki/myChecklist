4096acb4c35bfa5d645a6f068cac05ae73c628c1
elboraki <younes.elboraki@gmail.com>
2020-01-31 14:03:07 +0100

    add edit and delete and show item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,32 @@
 class ItemsController < ApplicationController
+
+    before_action :item_find , only: [:show,:update,:edit,:destroy]
+
     def index
         @items=Item.all.order("created_at DESC")
     end
 
     def new
         @item=Item.new
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @item.update(item_params)
+            redirect_to item_path(@item)
+        else
+            render edit_item_path(@item) 
+        end
+    end
+
+    def destroy
+        @item.destroy
+        redirect_to root_path
     end
 
     def create
@@ -19,5 +41,9 @@ class ItemsController < ApplicationController
     private
      def item_params
         params.require(:item).permit(:title,:description)
+     end
+
+     def item_find
+        @item=Item.find(params[:id])
      end
 end
